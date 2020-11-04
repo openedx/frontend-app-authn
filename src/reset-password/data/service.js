7af8c5b4822965dcbc995b707c73a5014d1e420e
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2020-11-04 21:24:08 +0500

    Add backend validation on password reset page.
    On old password reset page we use backend endpoint to validate password,
    added same functionality for the MFE.
    
    VAN-87

--- src/reset-password/data/service.js
+++ src/reset-password/data/service.js
@@ -1,5 +1,5 @@
 import { getConfig } from '@edx/frontend-platform';
-import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
+import { getHttpClient, getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 import formurlencoded from 'form-urlencoded';
 
 // eslint-disable-next-line import/prefer-default-export
@@ -38,4 +38,28 @@ export async function resetPassword(payload, token) {
       throw (e);
     });
   return data;
+}
+
+export async function validatePassword(password) {
+  const requestConfig = {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  };
+  const { data } = await getHttpClient()
+    .post(
+      `${getConfig().LMS_BASE_URL}/api/user/v1/validation/registration`,
+      formurlencoded({ password }),
+      requestConfig,
+    ).then((resp) => resp)
+    .catch((e) => {
+      throw (e);
+    });
+
+  let errorMessage = '';
+  // Be careful about grabbing this message, since we could have received an HTTP error or the
+  // endpoint didn't give us what we expect. We only care if we get a clear error message.
+  if (data.validation_decisions && data.validation_decisions.password) {
+    errorMessage = data.validation_decisions.password;
+  }
+
+  return errorMessage;
 }
