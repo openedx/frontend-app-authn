0497fcb0f2416dc32330b7eb9d4fc1c1d341410e
uzairr <uzairr@yahoo.com>
2021-01-06 18:28:08 +0500

    fix spinner height

--- src/reset-password/Spinner.jsx
+++ src/reset-password/Spinner.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Spinner as ParagonSpinner } from '@edx/paragon';
 
 const Spinner = () => (
-  <div className="container position-absolute h-100">
-    <div className="d-flex justify-content-center align-items-center h-100">
+  <div className="container position-absolute h-90">
+    <div className="d-flex justify-content-center align-items-center h-90">
       <ParagonSpinner animation="border" variant="primary" />
     </div>
   </div>
