085a53fe24c05c046d8b73423e1e28e94c0f0592
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2021-02-22 15:41:55 +0500

    Forgot password endpoint require a CSRF token.

--- src/forgot-password/data/service.js
+++ src/forgot-password/data/service.js
@@ -1,14 +1,15 @@
 import { getConfig } from '@edx/frontend-platform';
-import { getHttpClient } from '@edx/frontend-platform/auth';
+import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 import formurlencoded from 'form-urlencoded';
 
 // eslint-disable-next-line import/prefer-default-export
 export async function forgotPassword(email) {
   const requestConfig = {
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    isPublic: true,
   };
 
-  const { data } = await getHttpClient()
+  const { data } = await getAuthenticatedHttpClient()
     .post(
       `${getConfig().LMS_BASE_URL}/account/password`,
       formurlencoded({ email }),
