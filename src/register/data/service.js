123ceede335a6607dc450495a42822b9a17999ae
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2021-02-22 16:44:18 +0500

    Fix registration process.
    getHttpClient doesn't seems to be working as expected, changing it
    back to getAuthenticatedHttpClient.

--- src/register/data/service.js
+++ src/register/data/service.js
@@ -1,13 +1,14 @@
 import { getConfig } from '@edx/frontend-platform';
-import { getHttpClient } from '@edx/frontend-platform/auth';
+import { getHttpClient, getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
 import querystring from 'querystring';
 
 export async function registerRequest(registrationInformation) {
   const requestConfig = {
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    isPublic: true,
   };
 
-  const { data } = await getHttpClient()
+  const { data } = await getAuthenticatedHttpClient()
     .post(
       `${getConfig().LMS_BASE_URL}/user_api/v2/account/registration/`,
       querystring.stringify(registrationInformation),
