78aa796575f60ac94058ae467a800b94e4d33b89
Zainab Amir <zainab.amir@arbisoft.com>
2021-04-07 15:23:05 +0500

    Make optional fields configurable (#234)
    Making optional fields configurable will help us in enabling and disabling
    fields as required.
    
    VAN-405

--- src/register/OptionalFields.jsx
+++ src/register/OptionalFields.jsx
@@ -9,7 +9,9 @@ import messages from './messages';
 import { AuthnValidationFormGroup } from '../common-components';
 
 const OptionalFields = (props) => {
-  const { intl, onChangeHandler, values } = props;
+  const {
+    intl, optionalFields, onChangeHandler, values,
+  } = props;
 
   const getOptions = () => ({
     yearOfBirthOptions: [{
@@ -28,59 +30,65 @@ const OptionalFields = (props) => {
 
   return (
     <>
-      <AuthnValidationFormGroup
-        label={intl.formatMessage(messages['registration.field.gender.options.label'])}
-        for="gender"
-        name="gender"
-        type="select"
-        key="gender"
-        value={values.gender}
-        className="mb-20 opt-inline-field data-hj-suppress"
-        onChange={(e) => onChangeHandler('gender', e.target.value)}
-        selectOptions={getOptions().genderOptions}
-        inputFieldStyle="border-gray-600 custom-select-size"
-      />
-      <AuthnValidationFormGroup
-        label={intl.formatMessage(messages['registration.year.of.birth.label'])}
-        for="yearOfBirth"
-        name="yearOfBirth"
-        type="select"
-        key="yearOfBirth"
-        value={values.yearOfBirth}
-        className="mb-20 opt-inline-field opt-year-field data-hj-suppress"
-        onChange={(e) => onChangeHandler('yearOfBirth', e.target.value)}
-        selectOptions={getOptions().yearOfBirthOptions}
-        inputFieldStyle="border-gray-600 custom-select-size"
-      />
-      <AuthnValidationFormGroup
-        label={intl.formatMessage(messages['registration.field.education.levels.label'])}
-        for="levelOfEducation"
-        name="levelOfEducation"
-        type="select"
-        key="levelOfEducation"
-        value={values.levelOfEducation}
-        className="mb-20 data-hj-suppress"
-        onChange={(e) => onChangeHandler('levelOfEducation', e.target.value)}
-        selectOptions={getOptions().educationLevelOptions}
-        inputFieldStyle="border-gray-600 custom-select-size"
-      />
-      <AuthnValidationFormGroup
-        label={intl.formatMessage(messages['registration.goals.label'])}
-        for="goals"
-        name="goals"
-        type="textarea"
-        key="goals"
-        value={values.goals}
-        className="mb-20"
-        onChange={(e) => onChangeHandler('goals', e.target.value)}
-        inputFieldStyle="border-gray-600 custom-select-size"
-      />
+      {optionalFields.includes('gender') && (
+        <AuthnValidationFormGroup
+          label={intl.formatMessage(messages['registration.field.gender.options.label'])}
+          for="gender"
+          name="gender"
+          type="select"
+          key="gender"
+          value={values.gender}
+          className="mb-20 data-hj-suppress"
+          onChange={(e) => onChangeHandler('gender', e.target.value)}
+          selectOptions={getOptions().genderOptions}
+        />
+      )}
+      {optionalFields.includes('yearOfBirth') && (
+        <AuthnValidationFormGroup
+          label={intl.formatMessage(messages['registration.year.of.birth.label'])}
+          for="yearOfBirth"
+          name="yearOfBirth"
+          type="select"
+          key="yearOfBirth"
+          value={values.yearOfBirth}
+          className="mb-20 data-hj-suppress"
+          onChange={(e) => onChangeHandler('yearOfBirth', e.target.value)}
+          selectOptions={getOptions().yearOfBirthOptions}
+        />
+      )}
+      {optionalFields.includes('levelOfEducation') && (
+        <AuthnValidationFormGroup
+          label={intl.formatMessage(messages['registration.field.education.levels.label'])}
+          for="levelOfEducation"
+          name="levelOfEducation"
+          type="select"
+          key="levelOfEducation"
+          value={values.levelOfEducation}
+          className="mb-20 data-hj-suppress"
+          onChange={(e) => onChangeHandler('levelOfEducation', e.target.value)}
+          selectOptions={getOptions().educationLevelOptions}
+        />
+      )}
+      {optionalFields.includes('goals') && (
+        <AuthnValidationFormGroup
+          label={intl.formatMessage(messages['registration.goals.label'])}
+          for="goals"
+          name="goals"
+          type="textarea"
+          key="goals"
+          value={values.goals}
+          className="mb-20"
+          onChange={(e) => onChangeHandler('goals', e.target.value)}
+          inputFieldStyle="border-gray-600"
+        />
+      )}
     </>
   );
 };
 
 OptionalFields.propTypes = {
   intl: intlShape.isRequired,
+  optionalFields: PropTypes.arrayOf(PropTypes.string).isRequired,
   onChangeHandler: PropTypes.func.isRequired,
   values: PropTypes.shape({
     gender: PropTypes.string,
