82f8e06c04f2e1ab7a6881b9b0857fa73656f1a6
Mubbshar Anwar <78487564+mubbsharanwar@users.noreply.github.com>
2021-04-13 12:59:30 +0500

    redesign base component (#232)
    * redesign base component
    
    * rebrand base component
    
    * redesign base component
    
    * rewrite & optimize base component
    
    * redesign base component
    
    * address feedback
    
    Co-authored-by: Waheed Ahmed <waheed.ahmed@arbisoft.com>

---
+++ src/common-components/MediumScreenHeader.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
+import { getConfig } from '@edx/frontend-platform';
+
+import messages from './messages';
+
+const MediumScreenHeader = (props) => {
+  const { intl } = props;
+
+  return (
+    <>
+      <div className="medium-screen-top-stripe" />
+      <div className="medium-screen-header">
+        <img alt="edx" className="logo" src={getConfig().LOGO_WHITE_URL} />
+        <div className="row mt-4">
+          <svg className="svg-line pl-5">
+            <line x1="50" y1="0" x2="10" y2="215" />
+          </svg>
+          <h1 className="large-heading pb-3">
+            {intl.formatMessage(messages['start.learning'])}
+            <span className="text-accent-a"><br />
+              {intl.formatMessage(messages['with.site.name'], { siteName: getConfig().SITE_NAME })}
+            </span>
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+MediumScreenHeader.propTypes = {
+  intl: intlShape.isRequired,
+};
+
+export default injectIntl(MediumScreenHeader);
