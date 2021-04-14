f9f645a1125421d63d0f4fd31309ec345330e2f4
adeelehsan <adeel.ehsan@arbisoft.com>
2021-04-15 04:35:12 +0500

    Cookie added for cta dialogue

--- src/common-components/RedirectLogistration.jsx
+++ src/common-components/RedirectLogistration.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Cookies from 'universal-cookie';
 
 import { getConfig } from '@edx/frontend-platform';
 
@@ -11,6 +12,9 @@ function RedirectLogistration(props) {
     // once user has successfully logged in. Otherwise, redirect to the specified redirect url.
     // Note: For multiple enterprise use case, we need to make sure that user first visits the
     // enterprise selection page and then complete the auth workflow
+
+    const cookies = new Cookies();
+    cookies.set('edx-show-cta-dialogue', true, { path: '/' });
     if (finishAuthUrl && !redirectUrl.includes(finishAuthUrl)) {
       window.location.href = getConfig().LMS_BASE_URL + finishAuthUrl;
     } else {
