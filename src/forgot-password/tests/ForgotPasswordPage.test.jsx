7b728289e7f1435b002283fa4cd55be6cb539552
adeelehsan <adeel.ehsan@arbisoft.com>
2021-05-06 02:52:14 +0500

    Forgot password page redesigned

--- src/forgot-password/tests/ForgotPasswordPage.test.jsx
+++ src/forgot-password/tests/ForgotPasswordPage.test.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { act } from 'react-dom/test-utils';
 import { Provider } from 'react-redux';
-import { Router } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import { mount } from 'enzyme';
 import configureStore from 'redux-mock-store';
-import { createMemoryHistory } from 'history';
 import { IntlProvider, injectIntl } from '@edx/frontend-platform/i18n';
 import CookiePolicyBanner from '@edx/frontend-component-cookie-policy-banner';
 import * as analytics from '@edx/frontend-platform/analytics';
@@ -19,7 +18,11 @@ analytics.sendPageEvent = jest.fn();
 
 const IntlForgotPasswordPage = injectIntl(ForgotPasswordPage);
 const mockStore = configureStore();
-const history = createMemoryHistory();
+const initialState = {
+  forgotPassword: {
+    status: null,
+  },
+};
 
 describe('ForgotPasswordPage', () => {
   let props = {};
@@ -27,12 +30,14 @@ describe('ForgotPasswordPage', () => {
 
   const reduxWrapper = children => (
     <IntlProvider locale="en">
-      <Provider store={store}>{children}</Provider>
+      <MemoryRouter>
+        <Provider store={store}>{children}</Provider>
+      </MemoryRouter>
     </IntlProvider>
   );
 
   beforeEach(() => {
-    store = mockStore();
+    store = mockStore(initialState);
     props = {
       forgotPassword: jest.fn(),
       status: null,
@@ -65,45 +70,29 @@ describe('ForgotPasswordPage', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  it('should match success section snapshot', () => {
-    props = {
-      ...props,
-      status: 'complete',
-    };
-    renderer.create(
-      reduxWrapper(
-        <Router history={history}>
-          <IntlForgotPasswordPage {...props} />
-        </Router>,
-      ),
-    );
-    expect(history.location.pathname).toEqual('/login');
-  });
-
   it('should display need other help signing in button', () => {
     const wrapper = mount(reduxWrapper(<IntlForgotPasswordPage {...props} />));
-    expect(wrapper.find('button.field-link').first().text()).toEqual('Need other help signing in?');
+    expect(wrapper.find('#forgot-password.btn-link').first().text()).toEqual('Need help signing in?');
   });
 
   it('should display email validation error message', async () => {
-    const validationMessage = "We couldn’t send the password recovery email.The email address you've provided isn't formatted correctly.";
+    const validationMessage = 'We were unable to contact you.Enter a valid email address below.';
     const wrapper = mount(reduxWrapper(<IntlForgotPasswordPage {...props} />));
 
-    wrapper.find('input#forgot-password-input').simulate(
+    wrapper.find('input#email').simulate(
       'change', { target: { value: 'invalid-email', name: 'email' } },
     );
-    await act(async () => { await wrapper.find('button.btn-primary').simulate('click'); });
+    await act(async () => { await wrapper.find('button.btn-brand').simulate('click'); });
     wrapper.update();
 
     expect(wrapper.find('.alert-danger').text()).toEqual(validationMessage);
   });
 
   it('should show alert on server error', () => {
-    props = {
-      ...props,
-      status: INTERNAL_SERVER_ERROR,
-    };
-    const expectedMessage = 'We couldn’t send the password recovery email.'
+    store = mockStore({
+      forgotPassword: { status: INTERNAL_SERVER_ERROR },
+    });
+    const expectedMessage = 'We were unable to contact you.'
                             + 'An error has occurred. Try refreshing the page, or check your internet connection.';
     const wrapper = mount(reduxWrapper(<IntlForgotPasswordPage {...props} />));
 
@@ -111,10 +100,10 @@ describe('ForgotPasswordPage', () => {
   });
 
   it('should display empty email validation message', async () => {
-    const validationMessage = 'We couldn’t send the password recovery email.Please enter your email.';
+    const validationMessage = 'We were unable to contact you.Enter your email below.';
     const forgotPasswordPage = mount(reduxWrapper(<IntlForgotPasswordPage {...props} />));
 
-    await act(async () => { await forgotPasswordPage.find('button.btn-primary').simulate('click'); });
+    await act(async () => { await forgotPasswordPage.find('button.btn-brand').simulate('click'); });
 
     forgotPasswordPage.update();
     expect(forgotPasswordPage.find('.alert-danger').text()).toEqual(validationMessage);
@@ -133,7 +122,7 @@ describe('ForgotPasswordPage', () => {
   it('should not display any error message on change event', () => {
     const forgotPasswordPage = mount(reduxWrapper(<IntlForgotPasswordPage {...props} />));
 
-    const emailInput = forgotPasswordPage.find('input#forgot-password-input');
+    const emailInput = forgotPasswordPage.find('input#email');
     emailInput.simulate('change', { target: { value: 'invalid-email', name: 'email' } });
     forgotPasswordPage.update();
 
@@ -141,20 +130,35 @@ describe('ForgotPasswordPage', () => {
   });
 
   it('should display error message on blur event', async () => {
-    const validationMessage = 'Please enter your email.';
+    const validationMessage = 'Enter your email';
     const forgotPasswordPage = mount(reduxWrapper(<IntlForgotPasswordPage {...props} />));
-    const emailInput = forgotPasswordPage.find('input#forgot-password-input');
+    const emailInput = forgotPasswordPage.find('input#email');
 
     await act(async () => {
       await emailInput.simulate('blur', { target: { value: '', name: 'email' } });
     });
 
     forgotPasswordPage.update();
-    expect(forgotPasswordPage.find('#forgot-password-input-invalid-feedback').text()).toEqual(validationMessage);
+    expect(forgotPasswordPage.find('.pgn__form-control-description-invalid').text()).toEqual(validationMessage);
   });
 
   it('check cookie rendered', () => {
     const forgotPage = mount(reduxWrapper(<IntlForgotPasswordPage {...props} />));
     expect(forgotPage.find(<CookiePolicyBanner />)).toBeTruthy();
+  });
+
+  it('should display success message after email is sent', async () => {
+    store = mockStore({
+      ...initialState,
+      forgotPassword: {
+        status: 'complete',
+      },
+    });
+    const successMessage = 'Check your emailWe sent an email to  with instructions to reset your password. If you do not '
+                           + 'receive a password reset message after 1 minute, verify that you entered the correct email address,'
+                           + ' or check your spam folder. If you need further assistance, contact technical support.';
+
+    const wrapper = mount(reduxWrapper(<IntlForgotPasswordPage {...props} />));
+    expect(wrapper.find('.alert-success').text()).toEqual(successMessage);
   });
 });
