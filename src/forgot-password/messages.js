df38c9e599403ff6d57e87099f7ad120c0bf2973
adeelehsan <adeel.ehsan@arbisoft.com>
2021-05-06 02:52:14 +0500

    Forgot password page redesigned

--- src/forgot-password/messages.js
+++ src/forgot-password/messages.js
@@ -8,19 +8,20 @@ const messages = defineMessages({
   },
   'forgot.password.page.heading': {
     id: 'forgot.password.page.heading',
-    defaultMessage: 'Password assistance',
+    defaultMessage: 'Reset password',
     description: 'The page heading for the forgot password page.',
   },
   'forgot.password.page.instructions': {
     id: 'forgot.password.page.instructions',
-    defaultMessage: 'Please enter your log-in or recovery email address below and we will send you an email with instructions.',
+    defaultMessage: 'Please enter your email address below and we will send you an email with instructions on how to reset your password.',
     description: 'Instructions message for forgot password page.',
   },
   'forgot.password.page.invalid.email.message': {
     id: 'forgot.password.page.invalid.email.message',
-    defaultMessage: "The email address you've provided isn't formatted correctly.",
-    description: 'Invalid email address message for the forgot password page.',
+    defaultMessage: 'Enter a valid email address',
+    description: 'Invalid email address message for input field.',
   },
+
   'forgot.password.page.email.field.label': {
     id: 'forgot.password.page.email.field.label',
     defaultMessage: 'Email',
@@ -28,12 +29,12 @@ const messages = defineMessages({
   },
   'forgot.password.page.submit.button': {
     id: 'forgot.password.page.submit.button',
-    defaultMessage: 'Recover my password',
+    defaultMessage: 'Submit',
     description: 'Submit button text for the forgot password page.',
   },
-  'forgot.password.request.server.error': {
-    id: 'forgot.password.request.server.error',
-    defaultMessage: 'We couldn’t send the password recovery email.',
+  'forgot.password.error.alert.title': {
+    id: 'forgot.password.error.alert.title.',
+    defaultMessage: 'We were unable to contact you.',
     description: 'Failed to send password recovery email.',
   },
   'forgot.password.error.message.title': {
@@ -48,7 +49,7 @@ const messages = defineMessages({
   },
   'forgot.password.empty.email.field.error': {
     id: 'forgot.password.empty.email.field.error',
-    defaultMessage: 'Please enter your email.',
+    defaultMessage: 'Enter your email',
     description: 'Error message that appears when user tries to submit empty email field',
   },
   'forgot.password.invalid.email.heading': {
@@ -66,5 +67,38 @@ const messages = defineMessages({
     defaultMessage: 'The email address you used to register with {platformName}',
     description: 'text help for the email',
   },
+  // Confirmation Alert Message
+  'confirmation.message.title': {
+    id: 'confirmation.message.title',
+    defaultMessage: 'Check your email',
+    description: 'Forgot password confirmation message title',
+  },
+  'confirmation.support.link': {
+    id: 'confirmation.support.link',
+    defaultMessage: 'contact technical support',
+    description: 'Technical support link text',
+  },
+
+  'need.help.sign.in.text': {
+    id: 'need.help.sign.in.text',
+    defaultMessage: 'Need help signing in?',
+    description: 'Sign in help link on forgot password page',
+  },
+  'additional.help.text': {
+    id: 'additional.help.text',
+    defaultMessage: 'For additional help, contact edX support at {infoEmail}',
+    description: 'additional help text on forgot password page',
+  },
+  'sign.in.text': {
+    id: 'sign.in.text',
+    defaultMessage: 'Sign In',
+    description: 'login page link on password page',
+  },
+  'extend.field.errors': {
+    id: 'extend.field.errors',
+    defaultMessage: ' below.',
+    description: 'extends the field error for alert message',
+  },
+
 });
 export default messages;
