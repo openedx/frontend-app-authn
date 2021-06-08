98e506e0a4934b77cbe7b9f30e8af7de914ad664
Shafqat Farhan <shafqat.farhan@arbisoft.com>
2021-06-08 16:03:52 +0500

    VAN-476 - Fixed arc issues with safari browser (#323)

--- src/common-components/LargeScreenLayout.jsx
+++ src/common-components/LargeScreenLayout.jsx
@@ -6,13 +6,13 @@ import LargeScreenLeftLayout from './LargeScreenLeftLayout';
 
 const LargeScreenLayout = () => (
   <div className="container row p-0 m-0 large-screen-container">
-    <div className="col-md-8 p-0 screen-header">
+    <div className="col-md-9 p-0 screen-header">
       <img alt="edx" className="logo position-absolute" src={getConfig().LOGO_WHITE_URL} />
       <LargeScreenLeftLayout />
     </div>
-    <div className="col-md-4 p-0 screen-polygon">
+    <div className="col-md-3 p-0 screen-polygon">
       <svg width="100%" height="100%" className="ml-n1 large-screen-svg" preserveAspectRatio="xMaxYMin meet">
-        <g transform="skewX(168)">
+        <g transform="skewX(171.6)">
           <rect x="0" y="0" height="100%" width="100%" />
         </g>
       </svg>
