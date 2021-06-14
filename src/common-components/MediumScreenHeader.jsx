2be23c1de6efabe3f56025db257aa2bb6bd05b46
Shafqat Farhan <shafqat.farhan@arbisoft.com>
2021-06-14 16:56:17 +0500

    VAN-552 - Adjust medium screen header height (#339)

--- src/common-components/MediumScreenHeader.jsx
+++ src/common-components/MediumScreenHeader.jsx
@@ -10,9 +10,9 @@ const MediumScreenHeader = (props) => {
 
   return (
     <div className="container row p-0 m-0 medium-screen-container">
-      <div className="col-md-10 p-0 screen-header mb-4">
+      <div className="col-md-10 p-0 screen-header">
         <img alt="edx" className="logo" src={getConfig().LOGO_WHITE_URL} />
-        <div className="row mt-4">
+        <div className="row mt-4 justify-content-center">
           <svg className="medium-screen-svg-line pl-5">
             <line x1="50" y1="0" x2="10" y2="215" />
           </svg>
@@ -24,7 +24,7 @@ const MediumScreenHeader = (props) => {
           </h1>
         </div>
       </div>
-      <div className="col-md-2 p-0 screen-polygon mb-4">
+      <div className="col-md-2 p-0 screen-polygon">
         <svg width="100%" height="100%" className="medium-screen-svg" preserveAspectRatio="xMaxYMin meet">
           <g transform="skewX(168)">
             <rect x="0" y="0" height="100%" width="100%" />
