1f57b038aa615319081b57b3922a8fc88faf8c31
Zainab Amir <zainab.amir@arbisoft.com>
2021-07-08 14:56:44 +0500

    fix: remove bullets from error message (#388)

--- src/redesign/common-components/index.jsx
+++ src/redesign/common-components/index.jsx
@@ -7,7 +7,6 @@ export { default as SocialAuthProviders } from './SocialAuthProviders';
 export { default as ThirdPartyAuthAlert } from './ThirdPartyAuthAlert';
 export { default as InstitutionLogistration } from './InstitutionLogistration';
 export { RenderInstitutionButton } from './InstitutionLogistration';
-export { default as APIFailureMessage } from './APIFailureMessage';
 export { default as reducer } from './data/reducers';
 export { default as saga } from './data/sagas';
 export { storeName } from './data/selectors';
