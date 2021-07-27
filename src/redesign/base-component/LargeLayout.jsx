edx/frontend-app-logistration

feat: add progressive profiling

src/redesign/base-component/LargeLayout.jsx
@@ -3,18 +3,23 @@ import React from 'react';
 import { getConfig } from '@edx/frontend-platform';
 import { Hyperlink, Image } from '@edx/paragon';
 
-import LargeScreenLeftLayout from './LargeScreenLeftLayout';
+import LargeScreenLeftLayout from './LargeLeftLayout';
 
-const LargeScreenLayout = () => (
+const LargeLayout = () => (
   <div className="container row p-0 m-0 large-screen-container">
-    <div className="col-md-9 p-0 screen-header">
+    <div className="col-md-9 p-0 screen-header-primary">
       <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
         <Image alt="edx" className="logo position-absolute" src={getConfig().LOGO_WHITE_URL} />
       </Hyperlink>
       <LargeScreenLeftLayout />
     </div>
     <div className="col-md-3 p-0 screen-polygon">
-      <svg width="100%" height="100%" className="ml-n1 large-screen-svg" preserveAspectRatio="xMaxYMin meet">
+      <svg
+        width="100%"
+        height="100%"
+        className="m1-n1 large-screen-svg-primary"
+        preserveAspectRatio="xMaxYMin meet"
+      >
         <g transform="skewX(171.6)">
           <rect x="0" y="0" height="100%" width="100%" />
         </g>
@@ -23,4 +28,4 @@ const LargeScreenLayout = () => (
   </div>
 );
 
-export default LargeScreenLayout;
+export default LargeLayout;

openedx/frontend-app-authn

src/redesign/base-component/LargeLayout.jsx
@@ -3,18 +3,23 @@ import React from 'react';
 import { getConfig } from '@edx/frontend-platform';
 import { Hyperlink, Image } from '@edx/paragon';
 
-import LargeScreenLeftLayout from './LargeScreenLeftLayout';
+import LargeScreenLeftLayout from './LargeLeftLayout';
 
-const LargeScreenLayout = () => (
+const LargeLayout = () => (
   <div className="container row p-0 m-0 large-screen-container">
-    <div className="col-md-9 p-0 screen-header">
+    <div className="col-md-9 p-0 screen-header-primary">
       <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
         <Image alt="edx" className="logo position-absolute" src={getConfig().LOGO_WHITE_URL} />
       </Hyperlink>
       <LargeScreenLeftLayout />
     </div>
     <div className="col-md-3 p-0 screen-polygon">
-      <svg width="100%" height="100%" className="ml-n1 large-screen-svg" preserveAspectRatio="xMaxYMin meet">
+      <svg
+        width="100%"
+        height="100%"
+        className="ml-n1 large-screen-svg-primary"
+        preserveAspectRatio="xMaxYMin meet"
+      >
         <g transform="skewX(171.6)">
           <rect x="0" y="0" height="100%" width="100%" />
         </g>
@@ -23,4 +28,4 @@ const LargeScreenLayout = () => (
   </div>
 );
 
-export default LargeScreenLayout;
+export default LargeLayout;