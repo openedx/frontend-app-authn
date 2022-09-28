2cd32f124fe410d21313a044afbbd5213f5c72da
attiyaishaque <atiya.ishaq@arbisoft.com>
2022-09-28 19:18:07 +0500

    fix: Add yellow slanted line back for edX only

--- src/base-component/MediumLayout.jsx
+++ src/base-component/MediumLayout.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { getConfig } from '@edx/frontend-platform';
 import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
 import { Hyperlink, Image } from '@edx/paragon';
+import classNames from 'classnames';
 
 import messages from './messages';
 
@@ -14,13 +15,22 @@ const MediumLayout = ({ intl }) => (
         <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
           <Image alt={getConfig().SITE_NAME} className="logo" src={getConfig().LOGO_WHITE_URL} />
         </Hyperlink>
-        <h1 className="display-1 text-white mt-5 mb-5 ml-4.5 mr-2">
-          {intl.formatMessage(messages['start.learning'])}
-          <span className="text-accent-a ml-2">
-            {intl.formatMessage(messages['with.site.name'], { siteName: getConfig().SITE_NAME })}
-          </span>
-        </h1>
-        <div />
+        <div className="d-flex align-items-center justify-content-center mb-4 ">
+          <div className={classNames({ 'mt-1 medium-yellow-line': getConfig().SITE_NAME === 'edX' })} />
+          <div>
+            <h1
+              className={classNames(
+                'display-1 text-white mt-5 mb-5 mr-2',
+                { 'ml-4.5': getConfig().SITE_NAME !== 'edX' },
+              )}
+            >
+              <span className="mr-2">{intl.formatMessage(messages['start.learning'])}</span>
+              <span className="text-accent-a d-inline-block">
+                {intl.formatMessage(messages['with.site.name'], { siteName: getConfig().SITE_NAME })}
+              </span>
+            </h1>
+          </div>
+        </div>
       </div>
       <div className="col-md-2 bg-white p-0">
         <svg className="w-100 h-100 medium-screen-svg-primary" preserveAspectRatio="xMaxYMin meet">
