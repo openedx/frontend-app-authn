525abe6b886e887787523a0ecfebfdf473669ae6
Zainab Amir <zainab.amir@arbisoft.com>
2023-01-06 10:49:47 +0500

    feat: update configs (#721)

--- src/index.jsx
+++ src/index.jsx
@@ -10,6 +10,7 @@ import {
 import { ErrorPage } from '@edx/frontend-platform/react';
 import { messages as paragonMessages } from '@edx/paragon';
 
+import configuration from './config';
 import appMessages from './i18n';
 import MainApp from './MainApp';
 
@@ -27,26 +28,7 @@ subscribe(APP_INIT_ERROR, (error) => {
 initialize({
   handlers: {
     config: () => {
-      mergeConfig({
-        LOGIN_ISSUE_SUPPORT_LINK: process.env.LOGIN_ISSUE_SUPPORT_LINK || null,
-        ACTIVATION_EMAIL_SUPPORT_LINK: process.env.ACTIVATION_EMAIL_SUPPORT_LINK || null,
-        PASSWORD_RESET_SUPPORT_LINK: process.env.PASSWORD_RESET_SUPPORT_LINK || null,
-        TOS_AND_HONOR_CODE: process.env.TOS_AND_HONOR_CODE || null,
-        TOS_LINK: process.env.TOS_LINK || null,
-        PRIVACY_POLICY: process.env.PRIVACY_POLICY || null,
-        USER_SURVEY_COOKIE_NAME: process.env.USER_SURVEY_COOKIE_NAME || null,
-        COOKIE_DOMAIN: process.env.COOKIE_DOMAIN,
-        AUTHN_PROGRESSIVE_PROFILING_SUPPORT_LINK: process.env.AUTHN_PROGRESSIVE_PROFILING_SUPPORT_LINK || null,
-        DISABLE_ENTERPRISE_LOGIN: process.env.DISABLE_ENTERPRISE_LOGIN || '',
-        INFO_EMAIL: process.env.INFO_EMAIL || '',
-        REGISTER_CONVERSION_COOKIE_NAME: process.env.REGISTER_CONVERSION_COOKIE_NAME || null,
-        ENABLE_PROGRESSIVE_PROFILING_ON_AUTHN: process.env.ENABLE_PROGRESSIVE_PROFILING_ON_AUTHN || false,
-        MARKETING_EMAILS_OPT_IN: process.env.MARKETING_EMAILS_OPT_IN || '',
-        ENABLE_COPPA_COMPLIANCE: process.env.ENABLE_COPPA_COMPLIANCE || '',
-        ENABLE_DYNAMIC_REGISTRATION_FIELDS: process.env.ENABLE_DYNAMIC_REGISTRATION_FIELDS || false,
-        SHOW_CONFIGURABLE_EDX_FIELDS: process.env.SHOW_CONFIGURABLE_EDX_FIELDS || false,
-        ENABLE_COOKIE_POLICY_BANNER: process.env.ENABLE_COOKIE_POLICY_BANNER || false,
-      });
+      mergeConfig(configuration);
     },
   },
   messages: [
