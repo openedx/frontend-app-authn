fbb8864aab69401dfb038e5b4170619f15ccf2f0
Yoiber Beitar <yoiber.beitar@edunext.co>
2023-02-13 17:20:05 -0500

    chore(i18n): add more languages

--- src/i18n/index.jsx
+++ src/i18n/index.jsx
@@ -1,41 +1,34 @@
 import arMessages from './messages/ar.json';
-import caMessages from './messages/ca.json';
-// no need to import en messages-- they are in the defaultMessage field
-import dedeMessages from './messages/de_DE.json';
-import es419Messages from './messages/es_419.json';
 import frMessages from './messages/fr.json';
-import heMessages from './messages/he.json';
-import hiMessages from './messages/hi.json';
-import idMessages from './messages/id.json';
-import ititMessages from './messages/it_IT.json';
-import kokrMessages from './messages/ko_kr.json';
-import plMessages from './messages/pl.json';
-import ptbrMessages from './messages/pt_br.json';
-import ptptMessages from './messages/pt_PT.json';
-import ruMessages from './messages/ru.json';
-import thMessages from './messages/th.json';
-import ukMessages from './messages/uk.json';
+import es419Messages from './messages/es_419.json';
 import zhcnMessages from './messages/zh_CN.json';
+import ptMessages from './messages/pt.json';
+import itMessages from './messages/it.json';
+import ukMessages from './messages/uk.json';
+import deMessages from './messages/de.json';
+import ruMessages from './messages/ru.json';
+import hiMessages from './messages/hi.json';
+import frCAMessages from './messages/fr_CA.json';
+import dedeCAMessages from './messages/de_DE.json';
+import ititCAMessages from './messages/it_IT.json';
+import ptptCAMessages from './messages/pt_PT.json';
+// no need to import en messages-- they are in the defaultMessage field
 
 const messages = {
   ar: arMessages,
-  es: es419Messages, // Prospectus uses es language code for spanish, added `es` option and pointed to es-419 strings.
   'es-419': es419Messages,
   fr: frMessages,
   'zh-cn': zhcnMessages,
-  'it-it': ititMessages,
-  'pt-pt': ptptMessages,
-  'de-de': dedeMessages,
-  ca: caMessages,
-  he: heMessages,
-  id: idMessages,
-  'ko-kr': kokrMessages,
-  pl: plMessages,
-  'pt-br': ptbrMessages,
-  ru: ruMessages,
-  th: thMessages,
-  uk: ukMessages,
+  pt: ptMessages,
+  it: itMessages,
+  de: deMessages,
   hi: hiMessages,
+  'fr-ca': frCAMessages,
+  ru: ruMessages,
+  uk: ukMessages,
+  'de-de': dedeCAMessages,
+  'it-it': ititCAMessages,
+  'pt-pt': ptptCAMessages
 };
 
 export default messages;
