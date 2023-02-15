b44064c3fb76db0c170ce4e958b05dcdb4b39aac
alipov-dm <dmytro.alipov@raccoongang.com>
2023-02-15 21:46:12 +0200

    fix: display or sign in with
    Fixed missing "Or sign in with" display in
    some cases. Added tests for different cases.

--- src/login/LoginPage.jsx
+++ src/login/LoginPage.jsx
@@ -170,12 +170,12 @@ class LoginPage extends React.Component {
 
     return (
       <>
-        {((!isEnterpriseLoginDisabled && isSocialAuthActive) || (isEnterpriseLoginDisabled && isInstitutionAuthActive))
-           && (
-             <div className="mt-4 mb-3 h4">
-               {intl.formatMessage(messages['login.other.options.heading'])}
-             </div>
-           )}
+        {(isSocialAuthActive || (isEnterpriseLoginDisabled && isInstitutionAuthActive))
+          && (
+            <div className="mt-4 mb-3 h4">
+              {intl.formatMessage(messages['login.other.options.heading'])}
+            </div>
+          )}
 
         {(!isEnterpriseLoginDisabled && isSocialAuthActive) && (
           <Hyperlink className="btn btn-link btn-sm text-body p-0 mb-4" destination={this.getEnterPriseLoginURL()}>
