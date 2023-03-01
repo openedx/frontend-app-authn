2a69e07733d004d1d35a06594d56a5aa15377ad7
Zainab Amir <zainab.amir@arbisoft.com>
2023-03-01 15:38:24 +0500

    feat: add support for fallback recs in case of error

--- src/recommendations/RecommendationsPage.jsx
+++ src/recommendations/RecommendationsPage.jsx
@@ -6,6 +6,7 @@ import {
   Hyperlink, Image, Spinner, StatefulButton,
 } from '@edx/paragon';
 import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
 
 import { DEFAULT_REDIRECT_URL } from '../data/constants';
 import { EDUCATION_LEVEL_MAPPING, RECOMMENDATIONS_COUNT } from './data/constants';
@@ -27,6 +28,7 @@ const RecommendationsPage = (props) => {
 
   useEffect(() => {
     if (registrationResponse) {
+      const generalRecommendations = JSON.parse(getConfig().GENERAL_RECOMMENDATIONS);
       let coursesWithKeys = [];
       getPersonalizedRecommendations(educationLevel).then((response) => {
         coursesWithKeys = response.map(course => ({
@@ -37,7 +39,7 @@ const RecommendationsPage = (props) => {
         if (coursesWithKeys.length >= RECOMMENDATIONS_COUNT) {
           setRecommendations(coursesWithKeys.slice(0, RECOMMENDATIONS_COUNT));
         } else {
-          const courseRecommendations = coursesWithKeys.concat(getConfig().GENERAL_RECOMMENDATIONS);
+          const courseRecommendations = coursesWithKeys.concat(generalRecommendations);
           // Remove duplicate recommendations
           const uniqueRecommendations = courseRecommendations.filter(
             (recommendation, index, self) => index === self.findIndex((existingRecommendation) => (
@@ -48,13 +50,14 @@ const RecommendationsPage = (props) => {
         }
 
         setIsLoading(false);
-        // We only want to track the recommendations returned by Algolia
-        const courseKeys = coursesWithKeys.map(course => course.courseKey);
-        trackRecommendationsViewed(courseKeys, false, userId);
       })
         .catch(() => {
+          setRecommendations(generalRecommendations.slice(0, RECOMMENDATIONS_COUNT));
           setIsLoading(false);
         });
+      // We only want to track the recommendations returned by Algolia
+      const courseKeys = coursesWithKeys.map(course => course.courseKey);
+      trackRecommendationsViewed(courseKeys, false, userId);
     }
   }, [registrationResponse, DASHBOARD_URL, educationLevel, userId]);
 
@@ -82,37 +85,44 @@ const RecommendationsPage = (props) => {
   };
 
   return (
-    <div className="d-flex flex-column vh-100">
-      <div className="mb-2">
-        <div className="col-md-12 small-screen-top-stripe medium-screen-top-stripe extra-large-screen-top-stripe" />
-        <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
-          <Image className="logo" alt={getConfig().SITE_NAME} src={getConfig().LOGO_URL} />
-        </Hyperlink>
-      </div>
-      {(!isLoading && recommendations.length === RECOMMENDATIONS_COUNT) ? (
-        <div className="d-flex flex-column align-items-center justify-content-center flex-grow-1 p-1">
-          <RecommendationsList
-            title={intl.formatMessage(messages['recommendation.page.heading'])}
-            recommendations={recommendations}
-            userId={userId}
-          />
-          <div className="text-center">
-            <StatefulButton
-              className="font-weight-500"
-              type="submit"
-              variant="brand"
-              labels={{
-                default: intl.formatMessage(messages['recommendation.skip.button']),
-              }}
-              onClick={handleSkip}
-            />
-          </div>
+    <>
+      <Helmet>
+        <title>{intl.formatMessage(messages['recommendation.page.title'],
+          { siteName: getConfig().SITE_NAME })}
+        </title>
+      </Helmet>
+      <div className="d-flex flex-column vh-100">
+        <div className="mb-2">
+          <div className="col-md-12 small-screen-top-stripe medium-screen-top-stripe extra-large-screen-top-stripe" />
+          <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
+            <Image className="logo" alt={getConfig().SITE_NAME} src={getConfig().LOGO_URL} />
+          </Hyperlink>
         </div>
-      )
-        : (
-          <Spinner animation="border" variant="primary" className="centered-align-spinner" />
-        )}
-    </div>
+        {(!isLoading && recommendations.length === RECOMMENDATIONS_COUNT) ? (
+          <div className="d-flex flex-column align-items-center justify-content-center flex-grow-1 p-1">
+            <RecommendationsList
+              title={intl.formatMessage(messages['recommendation.page.heading'])}
+              recommendations={recommendations}
+              userId={userId}
+            />
+            <div className="text-center">
+              <StatefulButton
+                className="font-weight-500"
+                type="submit"
+                variant="brand"
+                labels={{
+                  default: intl.formatMessage(messages['recommendation.skip.button']),
+                }}
+                onClick={handleSkip}
+              />
+            </div>
+          </div>
+        )
+          : (
+            <Spinner animation="border" variant="primary" className="centered-align-spinner" />
+          )}
+      </div>
+    </>
   );
 };
 
