21e6bb6eec59e1f806ce2f4d118e5265963bbba1
Zainab Amir <zainab.amir@arbisoft.com>
2023-07-27 15:04:56 +0500

    feat: update static recommendation placement (#1002)
    * feat: update static recommendation placement
    * refactor: clean recommendation code (#1003)
    
    ---------
    
    Co-authored-by: Syed Sajjad Hussain Shah <52817156+syedsajjadkazmii@users.noreply.github.com>

--- src/recommendations/RecommendationsList.jsx
+++ src/recommendations/RecommendationsList.jsx
@@ -1,79 +1,32 @@
 import React from 'react';
 
-import { useIntl } from '@edx/frontend-platform/i18n';
-import { Container, Dropdown, DropdownButton } from '@edx/paragon';
 import PropTypes from 'prop-types';
 
-import { RECOMMENDATIONS_OPTION_LIST } from './data/constants';
-import messages from './messages';
 import ProductCard from './ProductCard';
 
 const RecommendationsList = (props) => {
-  const { formatMessage } = useIntl();
-
-  const {
-    title, recommendations, userId, setSelectedRecommendationsType, selectedRecommendationsType,
-  } = props;
+  const { recommendations, userId } = props;
 
   return (
-    <Container id="course-recommendations" size="lg" className="recommendations-container">
-      <h2 className="text-sm-center mb-4 text-left recommendations-container__heading">
-        {title}
-      </h2>
-      <DropdownButton
-        id="dropdown-basic-button"
-        title={(
-          <>
-            {formatMessage(messages[`recommendation.option.${selectedRecommendationsType.value}`])}
-          </>
-        )}
-        className="bg-white mt-5.5 mb-3"
-      >
-        {RECOMMENDATIONS_OPTION_LIST.map((option) => (
-          <Dropdown.Item
-            onClick={() => setSelectedRecommendationsType(option)}
-            id={`option-${option.value}`}
-            key={`option-${option.value}`}
-          >
-            {formatMessage(messages[`recommendation.option.${option.value}`])}
-          </Dropdown.Item>
-        ))}
-      </DropdownButton>
-      <div className="d-flex recommendations-container__card-list">
-        {
-          recommendations.map((recommendation, idx) => (
+    <div className="d-flex recommendations-container__card-list">
+      {
+        recommendations.map((recommendation, idx) => (
+          <span key={recommendation.uuid}>
             <ProductCard
-              key={recommendation.activeRunKey}
               product={recommendation}
               position={idx}
               userId={userId}
             />
-          ))
-        }
-      </div>
-    </Container>
+          </span>
+        ))
+      }
+    </div>
   );
 };
 
 RecommendationsList.propTypes = {
-  title: PropTypes.string.isRequired,
-  setSelectedRecommendationsType: PropTypes.func.isRequired,
-  selectedRecommendationsType: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
-  }).isRequired,
   recommendations: PropTypes.arrayOf(PropTypes.shape({
-    courseKey: PropTypes.string.isRequired,
-    activeRunKey: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    cardImageUrl: PropTypes.string.isRequired,
-    owners: PropTypes.arrayOf(PropTypes.shape({
-      key: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      logoImageUrl: PropTypes.string.isRequired,
-    })),
-    marketingUrl: PropTypes.string.isRequired,
-    recommendationType: PropTypes.string,
+    uuid: PropTypes.string,
   })),
   userId: PropTypes.number,
 };
--- src/recommendations/tests/RecommendationsPage.test.jsx
+++ src/recommendations/tests/RecommendationsPage.test.jsx
@@ -4,11 +4,9 @@ import { Provider } from 'react-redux';
 import { getConfig, mergeConfig } from '@edx/frontend-platform';
 import { injectIntl, IntlProvider } from '@edx/frontend-platform/i18n';
 import { mount } from 'enzyme';
-import { act } from 'react-dom/test-utils';
 import configureStore from 'redux-mock-store';
 
 import { DEFAULT_REDIRECT_URL } from '../../data/constants';
-import getPersonalizedRecommendations from '../data/service';
 import RecommendationsPage from '../RecommendationsPage';
 
 const IntlRecommendationsPage = injectIntl(RecommendationsPage);
@@ -32,8 +30,11 @@ describe('RecommendationsPageTests', () => {
   let defaultProps = {};
   let store = {};
 
-  let registrationResult = {
-    redirectUrl: getConfig().LMS_BASE_URL.concat('/course-about-page-url'),
+  const dashboardUrl = getConfig().LMS_BASE_URL.concat(DEFAULT_REDIRECT_URL);
+  const redirectUrl = getConfig().LMS_BASE_URL.concat('/course-about-page-url');
+
+  const registrationResult = {
+    redirectUrl,
     success: true,
   };
   const reduxWrapper = children => (
@@ -41,16 +42,6 @@ describe('RecommendationsPageTests', () => {
       <Provider store={store}>{children}</Provider>
     </IntlProvider>
   );
-
-  const getRecommendationsPage = async (props = defaultProps) => {
-    const recommendationsPage = mount(reduxWrapper(<IntlRecommendationsPage {...props} />));
-    await act(async () => {
-      await Promise.resolve(recommendationsPage);
-      recommendationsPage.update();
-    });
-
-    return recommendationsPage;
-  };
 
   beforeEach(() => {
     store = mockStore({});
@@ -64,53 +55,25 @@ describe('RecommendationsPageTests', () => {
     };
   });
 
-  it('redirects to dashboard if user click on skip button', async () => {
-    const DASHBOARD_URL = getConfig().LMS_BASE_URL.concat(DEFAULT_REDIRECT_URL);
-    registrationResult = {
-      ...registrationResult,
-      redirectUrl: getConfig().LMS_BASE_URL.concat('/dashboard'),
-    };
-    const props = {
-      location: {
-        state: {
-          registrationResult,
-          userId: 111,
-        },
-      },
-    };
-    const recommendationsPage = await getRecommendationsPage(props);
+  it('should redirect to dashboard if user is not coming from registration workflow', () => {
+    mount(reduxWrapper(<IntlRecommendationsPage />));
+    expect(window.location.href).toEqual(dashboardUrl);
+  });
+
+  it('should redirect if either popular or trending recommendations are not configured', () => {
+    mount(reduxWrapper(<IntlRecommendationsPage {...defaultProps} />));
+    expect(window.location.href).toEqual(redirectUrl);
+  });
+
+  it('should redirect user if they click "Skip for now" button', () => {
+    const recommendationsPage = mount(reduxWrapper(<IntlRecommendationsPage {...defaultProps} />));
     recommendationsPage.find('.pgn__stateful-btn-state-default').first().simulate('click');
-    expect(window.location.href).toEqual(DASHBOARD_URL);
+
+    expect(window.location.href).toEqual(redirectUrl);
   });
 
-  it('should change the option when click the dropdown option', async () => {
-    const recommendationsPage = await getRecommendationsPage();
-
-    recommendationsPage.find('#dropdown-basic-button').at(1).simulate('click');
-    recommendationsPage.find('.dropdown-item').at(0).simulate('click',
-      { target: { title: 'Trending courses', value: 'trending' } });
-    expect(recommendationsPage.find('#dropdown-basic-button').at(1).text()).toEqual('Trending Courses');
-  });
-
-  it('should redirect if recommended courses count is less than RECOMMENDATIONS_COUNT', async () => {
-    delete window.location;
-    window.location = { assign: jest.fn() };
-    const recommendationsPage = await getRecommendationsPage();
-
-    expect(recommendationsPage.find('#course-recommendations').exists()).toBeTruthy();
-  });
-
-  it('redirects to dashboard if user tries to access the page directly', async () => {
-    const DASHBOARD_URL = getConfig().LMS_BASE_URL.concat(DEFAULT_REDIRECT_URL);
-    delete window.location;
-    window.location = {
-      href: getConfig().BASE_URL,
-      assign: jest.fn().mockImplementation((value) => { window.location.href = value; }),
-    };
-
-    await getRecommendationsPage({});
-
-    expect(getPersonalizedRecommendations).toHaveBeenCalledTimes(0);
-    expect(window.location.href).toEqual(DASHBOARD_URL);
+  it('displays popular products as default recommendations', () => {
+    const recommendationsPage = mount(reduxWrapper(<IntlRecommendationsPage {...defaultProps} />));
+    expect(recommendationsPage.find('.nav-link .active a').text()).toEqual('Most Popular');
   });
 });
