3d2850db4ca5dfcbdbea7994929895198d1e217c
Moncef Abboud <moncef.abboud95@gmail.com>
2023-10-02 11:54:44 +0200

    feat: implement SHOW_REGISTRATION_LINKS setting

--- src/common-components/EnterpriseSSO.jsx
+++ src/common-components/EnterpriseSSO.jsx
@@ -19,7 +19,8 @@ import { LOGIN_PAGE, SUPPORTED_ICON_CLASSES } from '../data/constants';
 const EnterpriseSSO = (props) => {
   const { formatMessage } = useIntl();
   const tpaProvider = props.provider;
-  const disablePublicAccountCreation = getConfig().ALLOW_PUBLIC_ACCOUNT_CREATION === false;
+  const hideRegistrationLink = getConfig().ALLOW_PUBLIC_ACCOUNT_CREATION === false
+    || getConfig().SHOW_REGISTRATION_LINKS === false;
 
   const handleSubmit = (e, url) => {
     e.preventDefault();
@@ -74,7 +75,7 @@ const EnterpriseSSO = (props) => {
                 className="w-100"
                 onClick={(e) => handleClick(e)}
               >
-                {disablePublicAccountCreation
+                {hideRegistrationLink
                   ? formatMessage(messages['enterprisetpa.login.button.text.public.account.creation.disabled'])
                   : formatMessage(messages['enterprisetpa.login.button.text'])}
               </Button>
