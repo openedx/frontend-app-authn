9a1477b4791d6054429afcb6f3d0a9f1239c6f14
Syed Sajjad  Hussain Shah <ssajjad@2u.com>
2024-03-13 10:35:52 +0500

    fix: fix clear backend error

--- src/register/RegistrationPage.jsx
+++ src/register/RegistrationPage.jsx
@@ -189,7 +189,9 @@ const RegistrationPage = (props) => {
   const handleOnChange = (event) => {
     const { name } = event.target;
     const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
-    dispatch(clearRegistrationBackendError(name));
+    if (registrationError[name]) {
+      dispatch(clearRegistrationBackendError(name));
+    }
     setErrors(prevErrors => ({ ...prevErrors, [name]: '' }));
     setFormFields(prevState => ({ ...prevState, [name]: value }));
   };
