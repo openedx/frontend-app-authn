966052a7f62ecc290b5d3ceadcb60063265ddcb9
Syed Sajjad Hussain Shah <52817156+syedsajjadkazmii@users.noreply.github.com>
2024-04-03 11:28:39 +0500

    feat: implement multi step registration experiment
    Rebase 2u main with master (#1228)
    
    * chore(deps): update dependency babel-plugin-formatjs to v10.5.14
    
    * fix(deps): update dependency @edx/frontend-platform to v7.1.3
    
    * fix(deps): update font awesome to v6.5.2
    
    * chore(deps): update dependency @openedx/frontend-build to v13.1.4
    
    * fix(deps): update dependency @openedx/paragon to v22.2.1
    
    * fix(deps): update dependency algoliasearch to v4.23.3
    
    * fix(deps): update dependency algoliasearch-helper to v3.17.0
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- src/register/components/tests/ThirdPartyAuth.test.jsx
+++ src/register/components/tests/ThirdPartyAuth.test.jsx
@@ -12,6 +12,9 @@ import configureStore from 'redux-mock-store';
 import {
   COMPLETE_STATE, LOGIN_PAGE, PENDING_STATE, REGISTER_PAGE,
 } from '../../../data/constants';
+import { NOT_INITIALIZED } from '../../data/optimizelyExperiment/helper';
+import useMultiStepRegistrationExperimentVariation
+  from '../../data/optimizelyExperiment/useMultiStepRegistrationExperimentVariation';
 import RegistrationPage from '../../RegistrationPage';
 
 jest.mock('@edx/frontend-platform/analytics', () => ({
@@ -22,6 +25,7 @@ jest.mock('@edx/frontend-platform/i18n', () => ({
   ...jest.requireActual('@edx/frontend-platform/i18n'),
   getLocale: jest.fn(),
 }));
+jest.mock('../../data/optimizelyExperiment/useMultiStepRegistrationExperimentVariation', () => jest.fn());
 
 const IntlRegistrationPage = injectIntl(RegistrationPage);
 const mockStore = configureStore();
@@ -120,6 +124,7 @@ describe('ThirdPartyAuth', () => {
       institutionLogin: false,
     };
     window.location = { search: '' };
+    useMultiStepRegistrationExperimentVariation.mockReturnValue(NOT_INITIALIZED);
   });
 
   afterEach(() => {
