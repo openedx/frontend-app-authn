23b6d4de721e764165f68eb65c6e5fad6a275fe4
Stanislav Lunyachek <stanislav.lunyachek@raccoongang.com>
2024-04-12 11:07:07 +0300

    feat: Hide preloaders for third party auth providers if they are disabled

--- src/login/LoginPage.jsx
+++ src/login/LoginPage.jsx
@@ -166,6 +166,7 @@ class LoginPage extends React.Component {
     const isInstitutionAuthActive = !!secondaryProviders.length && !currentProvider;
     const isSocialAuthActive = !!providers.length && !currentProvider;
     const isEnterpriseLoginDisabled = getConfig().DISABLE_ENTERPRISE_LOGIN;
+    const isThirdPartyAuthActive = isSocialAuthActive || (isEnterpriseLoginDisabled && isInstitutionAuthActive);
 
     return (
       <>
@@ -183,7 +184,7 @@ class LoginPage extends React.Component {
           </Hyperlink>
         )}
 
-        {thirdPartyAuthApiStatus === PENDING_STATE ? (
+        {thirdPartyAuthApiStatus === PENDING_STATE && isThirdPartyAuthActive ? (
           <Skeleton className="tpa-skeleton mb-3" height={30} count={2} />
         ) : (
           <>
