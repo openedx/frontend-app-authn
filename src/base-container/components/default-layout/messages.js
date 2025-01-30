90336d491d3e2a454302f4ae47858fd024a4a8e7
Cormilo <gitgitgithub@yandex.ru>
2025-01-30 17:10:03 +0000

    edited login site name

--- src/base-container/components/default-layout/messages.js
+++ src/base-container/components/default-layout/messages.js
@@ -3,7 +3,7 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   'start.learning': {
     id: 'start.learning',
-    defaultMessage: 'Start learning',
+    defaultMessage: 'PT Edtechlab',
     description: 'Header text for logistration MFE pages',
   },
   'with.site.name': {
