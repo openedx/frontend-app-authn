3601df00aee5d6680adcb971e0ddb1ba2940e1f8
Cormilo <gitgitgithub@yandex.ru>
2025-01-30 19:06:07 +0000

    Удалил логотип

--- src/base-container/components/image-layout/LargeLayout.jsx
+++ src/base-container/components/image-layout/LargeLayout.jsx
@@ -16,9 +16,6 @@ const LargeLayout = () => {
       className="w-50 bg-primary-500 banner__image large-layout"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
-        <Image className="company-logo position-absolute" alt={getConfig().SITE_NAME} src={getConfig().LOGO_WHITE_URL} />
-      </Hyperlink>
       <div className="min-vh-100 p-5 d-flex align-items-end">
         <h1 className="display-2 mw-sm mb-3 d-flex flex-column flex-shrink-0 justify-content-center">
           <span className="text-light-100">
