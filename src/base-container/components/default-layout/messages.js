c9beccc036f2a9e5e8ae25db98fc6c887b952ff0
techin18 <65010518024@msu.ac.th>
2025-02-20 15:41:33 +0700

    Update messages.js
    ตัวแก้

--- src/base-container/components/default-layout/messages.js
+++ src/base-container/components/default-layout/messages.js
@@ -3,12 +3,12 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   'start.learning': {
     id: 'KKU Academy',
-    defaultMessage: 'KKU Academy',
+    defaultMessage: '{siteName} Connecting The World',
     description: 'Header text for logistration MFE pages',
   },
   'with.site.name': {
     id: 'with.site.name',
-    defaultMessage: 'with {Connecting The World for Mastery Learning}',
+    defaultMessage: 'for Mastery Learning',
     description: 'Header text with site name for logistration MFE pages',
   },
 });
