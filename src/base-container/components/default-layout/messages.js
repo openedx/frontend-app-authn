b7ba004479ece04caf23e6325ec76296909a93ac
techin18 <65010518024@msu.ac.th>
2025-02-24 08:55:09 +0700

    messages.js

--- src/base-container/components/default-layout/messages.js
+++ src/base-container/components/default-layout/messages.js
@@ -3,12 +3,12 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   'start.learning': {
     id: 'start.learningy',
-    defaultMessage: 'KKU Academy Connecting The World',
+    defaultMessage: 'KKU Academy Connecting',
     description: 'Header text for logistration MFE pages',
   },
   'with.site.name': {
     id: 'with.site.name',
-    defaultMessage: 'for Mastery Learning',
+    defaultMessage: 'The World for Mastery Learning',
     description: '',
   },
 });
