af1cb8ce92290972627ba6a5730504133b52ce8c
techin18 <65010518024@msu.ac.th>
2025-03-07 09:23:15 +0700

    Update messages.js

--- src/base-container/components/default-layout/messages.js
+++ src/base-container/components/default-layout/messages.js
@@ -5,7 +5,6 @@ const messages = defineMessages({
     id: 'start.learningy',
     defaultMessage: 'Connecting with',
     description: '',
-    color: '$990000',
   },
   'with.site.name': {
     id: 'with.site.name',
