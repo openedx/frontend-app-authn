ce3d2ea85aa51070129409af33cb410642a84992
artur.filippovskii <arturfilippovskij@gmail.com>
2025-12-14 18:55:55 +0200

    refactor: replace magic number 255 with named constant

--- src/register/RegistrationFields/NameField/validator.js
+++ src/register/RegistrationFields/NameField/validator.js
@@ -9,13 +9,15 @@ export const HTML_REGEX = /<|>/u;
 // regex from backend
 export const INVALID_NAME_REGEX = /https?:\/\/(?:[-\w.]|(?:%[\da-fA-F]{2}))*/g;
 
+export const MAX_FULL_NAME_LENGTH = 255;
+
 const validateName = (value, formatMessage) => {
   let fieldError = '';
   if (!value.trim()) {
     fieldError = formatMessage(messages['empty.name.field.error']);
   } else if (URL_REGEX.test(value) || HTML_REGEX.test(value) || INVALID_NAME_REGEX.test(value)) {
     fieldError = formatMessage(messages['name.validation.message']);
-  } else if (value && value.length > 255) {
+  } else if (value && value.length > MAX_FULL_NAME_LENGTH) {
     fieldError = formatMessage(messages['name.validation.length.message']);
   }
   return fieldError;
